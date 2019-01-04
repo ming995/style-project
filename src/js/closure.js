333651ffd7acca8866f6c09627c7b8da545e29ea
ming995 <hymghost@gmail.com>
2019-01-04 13:22:49 +0800

    [feat]: 完成首页

---
+++ src/js/closure.js
@@ -0,0 +1,17 @@
+/**
+ *  闭包
+ *  https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8
+ */
+function createCounter() {
+    let counter = 0
+    const myFunction = function () {
+        counter = counter + 1
+        return counter
+    }
+    return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3, increment);
